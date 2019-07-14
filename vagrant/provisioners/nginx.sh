46caf14b9c8c109759578bb939d4026c10971031
Clinton Andrews <clinton@ignitenet.com>
2019-07-13 20:48:09 -0400

    Rewrite /api rules to api server

--- vagrant/provisioners/nginx.sh
+++ vagrant/provisioners/nginx.sh
@@ -29,6 +29,11 @@ server {
     rewrite ^/auth/(.*) /\$1 break;
     proxy_pass http://127.0.0.1:3000;
   }
+
+  location /api {
+    rewrite ^/api/(.*) /\$1 break;
+    proxy_pass http://127.0.0.1:3000;
+  }
 }
 
 EOF
